c3621333cafb5fd52b3fb371845a0f4dabdf2c4c
seonghochoigo <dsds550@gmail.com>
2019-03-13 17:09:33 +0900

    Project folder upload

---
+++ demo/src/main/java/com/example/demo/domain/TestDomain.java
@@ -0,0 +1,23 @@
+package com.example.demo.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Data
+@Entity
+public class TestDomain {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int testId;
+	
+	@Column(nullable=false)
+	private String content;
+	
+	
+}
